008f7822145c0a24cbab4eccdf4c125b70a7bbfb
sinjwu <harbirdouk@gmail.com>
2025-11-03 15:03:00 +0900

    First commit setup

---
+++ backend/src/main/java/com/team7/ConcerTUNE/ConcerTUNEBackend.java
@@ -0,0 +1,13 @@
+package com.team7.ConcerTUNE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcerTUNEBackend {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcerTUNEBackend.class, args);
+	}
+
+}
