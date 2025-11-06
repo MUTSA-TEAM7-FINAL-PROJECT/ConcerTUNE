fd60240e6c562fa0c0a5ab1077fb92e5c9b81d9b
sinjwu <harbirdouk@gmail.com>
2025-11-06 20:41:29 +0900

    18-프론트엔드-뼈대-및-아티스트-알림-구현

--- backend/src/main/java/com/team7/ConcerTUNE/dto/ArtistSummaryDto.java
+++ backend/src/main/java/com/team7/ConcerTUNE/dto/ArtistSummaryDto.java
@@ -1,4 +1,24 @@
 package com.team7.ConcerTUNE.dto;
 
+import com.team7.ConcerTUNE.entity.Artist;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ArtistSummaryDto {
+    private Long artistId;
+    private String artistName;
+    private String artistImageUrl;
+    private long followerCount;
+
+    public static ArtistSummaryDto fromEntity(Artist artist, long followerCount) {
+        return ArtistSummaryDto.builder()
+                .artistId(artist.getArtistId())
+                .artistName(artist.getArtistName())
+                .artistImageUrl(artist.getArtistImageUrl())
+                .followerCount(followerCount)
+                .build();
+    }
 }
--- backend/src/main/java/com/team7/ConcerTUNE/repository/NotificationRepository.java
+++ backend/src/main/java/com/team7/ConcerTUNE/repository/NotificationRepository.java
@@ -1,4 +1,7 @@
 package com.team7.ConcerTUNE.repository;
 
-public interface NotificationRepository {
+import com.team7.ConcerTUNE.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
 }
