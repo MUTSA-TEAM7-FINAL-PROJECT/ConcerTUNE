b838d11b8d306c4242bfca32d4bfe7200f72dd63
MadeByZino <jh990517@naver.com>
2025-11-07 16:02:05 +0900

    소셜유저 랜덤이름 길이 감소

--- backend/src/main/java/com/team7/ConcerTUNE/util/UsernameGenerator.java
+++ backend/src/main/java/com/team7/ConcerTUNE/util/UsernameGenerator.java
@@ -22,8 +22,8 @@ public class UsernameGenerator {
             String noun = NOUNS.get((int) (Math.random() * NOUNS.size()));
             String baseName = adj + noun;
 
-            int min = 100000;
-            int max = 999999;
+            int min = 1;
+            int max = 9999;
             int randomNumber = (int)(Math.random() * (max - min + 1)) + min;
 
             String uniqueUsername = baseName + randomNumber;
