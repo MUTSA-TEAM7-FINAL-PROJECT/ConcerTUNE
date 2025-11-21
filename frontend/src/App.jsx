fce352711afb487e326de0a01428227821a87ba5
USER <rhs9827@naver.com>
2025-11-21 17:25:48 +0900

    충돌

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,61 +28,73 @@ import SearchResultPage from "./pages/SearchResultPage"; // develop 브랜치
 import MyPage from "./pages/MyPage"; // develop 브랜치 (내 프로필 상세)
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<MainLayout />}>
-          <Route path="" element={<Home />} />
-          
-            {/* 검색 */}
-          <Route path="search" element={<SearchResultPage />} />
-
-            {/* 공연/아티스트 */}
-          <Route path="concerts" element={<ConcertListPage />} />
-          <Route path="concerts/:id" element={<ConcertDetailPage />} />
-          <Route path="concerts/request" element={<ConcertRequestPage />} />
-          <Route path="artists/:artistId" element={<ArtistDetailPage />} />
-          
-            {/* 요청 관리 (통합) */}
-          <Route path="admin/requests" element={<AdminRequestListPage />} />
-          <Route path="concerts/request-list" element={<RequestListPage />} />
-          <Route path="artists/request" element={<LiveRequestPage />} />
-          <Route path="artist-manager/requests" element={<ArtistManagerRequestPage />} />
-          <Route path="artist-manager/requests-list" element={<ArtistManagerRequestListPage />} />
-
-          {/* 커뮤니티 목록 및 탭 레이아웃 */}
-          <Route path="community" element={<CommunityPage />}>
-            <Route index element={<Navigate to="free" replace />} />
-            <Route path=":category" element={<PostList />} />
-          </Route>
-
-          {/* 게시글 상세 */}
-          <Route path="post/:postId" element={<PostDetail />} />
-
-          {/* 게시글 작성/수정 */}
-          <Route path="community/write/:category" element={<PostWriteEdit />} />
-          <Route
-            path="community/edit/:category/:postId"
-            element={<PostWriteEdit />}
-          />
-
-            {/* 사용자 프로필 (통합) */}
-            {/* MyPage와 ProfilePage가 겹치므로 MyPage를 개인 프로필(/me)로 지정하고 ProfilePage는 제거 (혹은 기능 통합) */}
-          <Route path="me" element={<MyPage />} />
-          <Route path="me/update" element={<ProfileUpdatePage />} />
-            {/* 타 사용자 프로필 */}
-          <Route path="user/:userId" element={<UserPage />} /> 
-        </Route>
-
-        {/* MainLayout을 사용하지 않는 페이지 */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/auth/select" element={<AuthSelectPage />} />
-        <Route path="/oauth2/callback" element={<OAuth2Callback />} />
-        <Route path="/password-reset" element={<PasswordResetPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return (
+    <BrowserRouter>
+           {" "}
+      <Routes>
+               {" "}
+        <Route path="/*" element={<MainLayout />}>
+                    <Route path="" element={<Home />} />         {/* 검색 */}
+                    <Route path="search" element={<SearchResultPage />} />
+          {/* 공연/아티스트 */}
+                    <Route path="concerts" element={<ConcertListPage />} />
+                   {" "}
+          <Route path="concerts/:id" element={<ConcertDetailPage />} />
+                   {" "}
+          <Route path="concerts/request" element={<ConcertRequestPage />} />
+                   {" "}
+          <Route path="artists/:artistId" element={<ArtistDetailPage />} />     
+             {/* 요청 관리 (통합) */}
+                   {" "}
+          <Route path="admin/requests" element={<AdminRequestListPage />} />
+                   {" "}
+          <Route path="concerts/request-list" element={<RequestListPage />} />
+                   {" "}
+          <Route path="artists/request" element={<LiveRequestPage />} />
+                   {" "}
+          <Route
+            path="artist-manager/requests"
+            element={<ArtistManagerRequestPage />}
+          />
+                   {" "}
+          <Route
+            path="artist-manager/requests-list"
+            element={<ArtistManagerRequestListPage />}
+          />
+                    {/* 커뮤니티 목록 및 탭 레이아웃 */}         {" "}
+          <Route path="community" element={<CommunityPage />}>
+                        <Route index element={<Navigate to="free" replace />} />
+                        <Route path=":category" element={<PostList />} />       
+             {" "}
+          </Route>
+                    {/* 게시글 상세 */}
+                    <Route path="post/:postId" element={<PostDetail />} />     
+              {/* 게시글 작성/수정 */}
+                   {" "}
+          <Route path="community/write/:category" element={<PostWriteEdit />} />
+                   {" "}
+          <Route
+            path="community/edit/:category/:postId"
+            element={<PostWriteEdit />}
+          />
+          {/* 사용자 프로필 (통합) */}
+          {/* MyPage와 ProfilePage가 겹치므로 MyPage를 개인 프로필(/me)로 지정하고 ProfilePage는 제거 (혹은 기능 통합) */}
+                    <Route path="me" element={<MyPage />} />
+                    <Route path="me/update" element={<ProfileUpdatePage />} />
+          {/* 타 사용자 프로필 */}
+                    <Route path="user/:userId" element={<UserPage />} />       {" "}
+        </Route>
+                {/* MainLayout을 사용하지 않는 페이지 */}
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/auth/select" element={<AuthSelectPage />} />
+                <Route path="/oauth2/callback" element={<OAuth2Callback />} />
+                <Route path="/password-reset" element={<PasswordResetPage />} />
+             {" "}
+      </Routes>
+         {" "}
+    </BrowserRouter>
+  );
 };
 
 export default App;
